2df5d787e1b831c0d8c89f09a3e67b4013aa0f1c
Jeff Jennings <jjennings1519@gmail.com>
2024-03-13 10:35:26 -0400

    logging

--- src/mpol/__init__.py
+++ src/mpol/__init__.py
@@ -1,2 +1,25 @@
 __version__ = "0.2.0"
 zenodo_record = 10064221
+
+def enable_logging(log_file=None):
+    """Turn on internal logging for MPoL
+
+    Parameters
+    ----------
+    log_file : string, optional
+        Output filename to which logging messages are written.
+        If not provided, logs will only be printed to the screen
+    """
+    import logging
+
+    if log_file is not None:
+        handlers = [ logging.FileHandler(log_file, mode='w'),
+                     logging.StreamHandler()
+                     ]
+    else:
+        handlers = [ logging.StreamHandler() ]
+
+    logging.basicConfig(level=logging.INFO,
+                        format='%(message)s',
+                        handlers=handlers
+                        )
--- src/mpol/constants.py
+++ src/mpol/constants.py
@@ -1,6 +1,9 @@
 import numpy as np
 from astropy.constants import c, k_B
 
+import mpol
+mpol.enable_logging()
+
 # convert from arcseconds to radians
 arcsec = np.pi / (180.0 * 3600)  # [radians]  = 1/206265 radian/arcsec
 
